29551325e07757c14c6129d7f2c2052ed0314eb6
Fabinho15 <51960922+Fabinho15@users.noreply.github.com>
2021-04-27 15:34:48 -0300

    Update Figura.cs

--- Figura.cs
+++ Figura.cs
@@ -1,19 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class Figura
 {
-    protected double _area; // atributo
-    protected double _perimetro; // atributo
+  protected double _area; // atributo
+  protected double _perimetro; // atributo
 
-    public double Area // propriedade
-    {
-        // leitura do atributo correspondente
-        get { return this._area; }
-    }
+  public double Area // propriedade
+  {
+    // leitura do atributo correspondente
+    get { return this._area; }
+  }
 
-    public double Perimetro // propriedade
-    {
-        // leitura do atributo correspondente
-        get { return this._perimetro; }
-    }
+  public double Perimetro // propriedade
+  {
+    // leitura do atributo correspondente
+    get { return this._perimetro; }
+  }
+
+  public void ImprimirFig()
+  {   
+    Console.WriteLine("Area:\t\t{0}", this._area);
+    Console.WriteLine("Perímetro:\t{0}", this._perimetro);
+  }
 }
